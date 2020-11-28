5e49efc96d80ccedd6f158f8670f295ba558fc82
Matttokin <55289178+Matttokin@users.noreply.github.com>
2020-11-28 18:59:00 +0300

    add govn0o

---
+++ WSR/DataBase/Models/OrderNom.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WSR.DataBase.Models
+{
+    public class OrderNom
+    {
+        public int Id { get; set; }
+        public Order Order { get; set; }
+        public Nomenclature Nomenclature { get; set; }
+        public int OrderId { get; set; }
+        public int NomenclatureId { get; set; }
+    }
+}
--- WSR/Global.asax.cs
+++ WSR/Global.asax.cs
@@ -18,6 +18,7 @@ namespace WSR
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            new Migrations.Configuration().startSeed();
         }
     }
 }
